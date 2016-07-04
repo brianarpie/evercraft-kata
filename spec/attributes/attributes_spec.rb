6b9f3bd2bbef668f5e18f3261a7e1e3451eefca8
Brian Arpie <brian.arpie@gmail.com>
2016-07-04 10:59:52 -0400

    feature: attack -- tests

--- spec/attributes/attributes_spec.rb
+++ spec/attributes/attributes_spec.rb
@@ -3,6 +3,6 @@ require 'attributes/alignment'
 
 describe Alignment do
 	it "is an enumerable w/ 'good', 'evil', & 'neutral'" do
-		expect(Alignment.new.entries).to eql(["good", "evil", "neutral"])
+		expect(Alignment.new.entries).to match_array ["good", "evil", "neutral"]
 	end
 end
--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -4,6 +4,7 @@ require 'attributes/alignment'
 
 describe Character do
 	wolverine = Character.new
+
 	describe ".name" do
 		context "given a name argument is set" do
 			wolverine.name = "Logan"
@@ -31,6 +32,39 @@ describe Character do
 		context "no armor class assigned" do
 			it "defaults to 10" do
 				expect(wolverine.armor_class()).to eq(10)
+			end
+		end
+	end
+	describe ".attack" do
+		context "with no enemy arg" do
+			it "raises an ArgumentError" do
+				expect { wolverine.attack() }.to raise_error ArgumentError
+			end
+		end
+		context "with non-Character enemy arg" do
+			it "raises a TypeError" do
+				expect { wolverine.attack(enemy="nightcrawler") }.to raise_error TypeError
+			end
+		end
+		context "with Character enemy arg" do
+			nightcrawler = Character.new
+
+			it "returns a roll value between 1 and 20" do
+				roll = wolverine.attack(enemy=nightcrawler)[:roll]
+				expect(roll).to be > 0 
+				expect(roll).to be <= 20
+			end
+			context "enemy armor_class is < die roll" do
+				it "renders a succesful attack" do
+					expect(wolverine.attack(enemy=nightcrawler, die_roll=20)[:success])
+						.to be true
+				end
+			end
+			context "enemy armor_class is >= die roll" do
+				it "renders an unsuccesful attack" do
+					expect(wolverine.attack(enemy=nightcrawler, die_roll=1)[:success])
+						.to be false
+				end
 			end
 		end
 	end
