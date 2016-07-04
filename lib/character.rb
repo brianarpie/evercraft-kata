7d8249dff0f9195f87b05d32d002ab6b9b415597
Brian Arpie <brian.arpie@gmail.com>
2016-07-04 11:00:01 -0400

    feature: attack -- complete

--- lib/character.rb
+++ lib/character.rb
@@ -14,7 +14,21 @@ class Character
 	def alignment=(alignment)
 		@alignment = Alignment.new.find { |x| x == alignment } 
 	end
+
 	def alignment
 		@alignment
+	end
+
+	def attack enemy, die_roll = nil
+		if !enemy.instance_of? Character
+			raise TypeError
+		end
+		die_roll ||= Random.new.rand(1..20)
+		if enemy.armor_class < die_roll
+			success = true
+		else
+			success = false
+		end
+		{ roll: die_roll, success: success }
 	end
 end
